c10389f1091a8ad4edeb77a8835eda0878b8cbfc
Lead0b110010100 <Lead0b110010100@outlook.com>
2020-04-20 00:27:54 +0200

    Added 'Reitkarte'.

--- main/srv1/share/locale/germany/quest/horse_summon.lua
+++ main/srv1/share/locale/germany/quest/horse_summon.lua
@@ -1,6 +1,6 @@
 quest horse_summon begin
 	state start begin
-		when 50053.use begin
+		when 50053.use or 50083.use begin
 			if horse.is_riding() then
 				horse.unride()
 			elseif horse.is_summon() then
