0d47b1e8388ec8019178f9e4089fabc77b20ed4b
Lead0b110010100 <Lead0b110010100@outlook.com>
2020-04-20 00:35:34 +0200

    Added destroy functionality for warehouse manager.

---
+++ main/srv1/share/locale/germany/quest/warehouse_item_destroyer.lua
@@ -0,0 +1,32 @@
+quest warehouse_item_destroyer begin
+	state start begin
+		when 9005.chat."Item zerstören Information" begin
+			say_title(mob_name(npc.get_race()).. ":")
+			say("")
+			say("Willkommen " ..pc.get_name().. ",")
+			say("Bei mir kannst du Items zerstören.")
+			say("Du kannst sogar einen ganzen Stapel zerstören.")
+			say("Zieh das Item einfach auf mich drauf.")
+			say("")
+		end
+		when 9005.take begin
+			local c = item.count
+			local i = item.vnum
+			say_title(mob_name(npc.get_race()).. ":")
+			say("")
+			
+				say("(GM) Vnum: " ..i)
+				say("(GM) ID: " ..item.get_id())
+		
+			if c <= 1 then
+				say_reward("Möchtest du " ..item_name(i).. " zerstören?")
+			else
+				say_reward("Möchtest du " ..c.. "x " ..item_name(i).. " zerstören?")
+			end
+			say("")
+			local s = select("Zerstören", "Abbrechen")
+			if s == 2 then return end
+			item.remove()
+		end
+	end
+end
