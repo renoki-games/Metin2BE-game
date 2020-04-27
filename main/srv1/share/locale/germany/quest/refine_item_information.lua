b7387b2da676b8ee48ffa389ff0e8989de8216ae
Lead0b110010100 <Lead0b110010100@outlook.com>
2020-04-27 03:23:18 +0200

    Added refine trigger.

--- main/srv1/share/locale/germany/quest/refine_item_information.lua
+++ main/srv1/share/locale/germany/quest/refine_item_information.lua
@@ -1,7 +1,7 @@
 quest refine_item_information begin
 	state start begin
 		when refine with item.get_level() == 9 begin
-			chat(string.format("%s %s |cffa7ffd4hat erfolgreich %s geuppt!|r", tag(SERVER_COLOR, "[Server]"), clickable_pn_tag(NAME_COLOR, pc.get_name()), clickable_item_tag(ITEM_COLOR, item.get_vnum(), item.get_name())))
+			notice_all(string.format("%s %s |cffa7ffd4hat erfolgreich %s geuppt!|r", tag(SERVER_COLOR, "[Server]"), clickable_pn_tag(NAME_COLOR, pc.get_name()), clickable_item_tag(ITEM_COLOR, item.get_vnum(), item.get_name())))
 		end
 	end
 end
